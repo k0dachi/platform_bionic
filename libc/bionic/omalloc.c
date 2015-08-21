6a26f942acbd9030ccfa06a887923a219cccd6d2
Daniel Micay <danielmicay@gmail.com>
2015-08-20 23:11:12 -0400

    only enable malloc_move by default with guards
    Moving large allocations to the end of the allocating space helps to
    uncover bugs but it's a liability when guard pages aren't enabled.

--- libc/bionic/omalloc.c
+++ libc/bionic/omalloc.c
@@ -593,7 +593,7 @@ omalloc_init(struct dir_info **dp)
 				mopts.malloc_move = 0;
 				break;
 			case 'P':
-				mopts.malloc_move = 1;
+				mopts.malloc_move = 2;
 				break;
 			case 'r':
 				mopts.malloc_realloc = 0;
@@ -633,6 +633,9 @@ omalloc_init(struct dir_info **dp)
 			}
 		}
 	}
+
+	if (mopts.malloc_move == 1 && !mopts.malloc_guard)
+		mopts.malloc_move = 0;
 
 #ifdef MALLOC_STATS
 	if (mopts.malloc_stats && (atexit(malloc_exit) == -1)) {
