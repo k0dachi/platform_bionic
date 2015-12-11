eaf7628005aa9ff6d9899f02321dc26ff59fdc98
Daniel Micay <danielmicay@gmail.com>
2015-12-10 21:19:23 -0500

    add __dynamic_object_size stack fast path
    This avoids calling __malloc_object_size for objects known to be on the
    current stack, and also provides a slight security bonus.

--- libc/bionic/__dynamic_object_size.cpp
+++ libc/bionic/__dynamic_object_size.cpp
@@ -26,10 +26,55 @@
  * SUCH DAMAGE.
  */
 
+#include <pthread.h>
 #include <stddef.h>
+#include <sys/cdefs.h>
+#include <unistd.h>
+
+#include "private/libc_logging.h"
+#include "pthread_internal.h"
 
 extern "C" size_t __malloc_object_size(const void*);
+int __pthread_attr_getstack_main_thread(void** stack_base, size_t* stack_size);
+
+static void* main_thread_stack_top = nullptr;
+
+static void main_thread_stack_top_init() {
+  if (__predict_false(main_thread_stack_top == nullptr)) {
+    void* current_base;
+    size_t current_size;
+    if (__pthread_attr_getstack_main_thread(&current_base, &current_size)) {
+      return;
+    }
+    main_thread_stack_top = static_cast<char*>(current_base) + current_size;
+  }
+}
 
 extern "C" size_t __dynamic_object_size(const void* ptr) {
+  pthread_internal_t* thread = __get_thread();
+
+  void* stack_base = nullptr;
+  void* stack_top = nullptr;
+  void* stack_frame = __builtin_frame_address(0);
+
+  if (thread->tid == getpid()) {
+    main_thread_stack_top_init();
+    if (__predict_true(main_thread_stack_top != nullptr)) {
+      stack_base = stack_frame;
+      stack_top = main_thread_stack_top;
+    }
+  } else {
+    stack_base = thread->attr.stack_base;
+    stack_top = static_cast<char*>(stack_base) + thread->attr.stack_size;
+  }
+
+  if (ptr > stack_base && ptr < stack_top) {
+    if (__predict_false(ptr < stack_frame)) {
+      __libc_fatal("%p is an invalid object address (in unused stack space %p-%p)", ptr, stack_base,
+                   stack_frame);
+    }
+    return static_cast<char*>(stack_top) - static_cast<char*>(const_cast<void*>(ptr));
+  }
+
   return __malloc_object_size(ptr);
 }
